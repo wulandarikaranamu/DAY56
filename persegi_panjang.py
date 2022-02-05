34a3b962f4eadbf0fcea61824d9631ccfa6d3623
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-02-05 22:14:55 +0700

    komentar

---
+++ persegi_panjang.py
@@ -0,0 +1,8 @@
+#menghitung luas dan keliling persegi panjang
+
+panjang = int(input("masukkan panjang :"))
+lebar = int(input("masukkan lebar :"))
+luas = panjang*lebar
+keliling = 2*(panjang + lebar)
+print("luasnya adalah :",luas)
+print("kelilingnya adalah :",keliling)
